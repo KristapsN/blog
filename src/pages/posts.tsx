202703c32db4da66c97e0b8d25ecd0747c8f1fe6
KristapsN <kristaps.nogobods@gmail.com>
2020-11-24 09:59:46 +0200

    next

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -6,107 +6,80 @@ import {
 import axios from 'axios'
 import "./posts.css"
 
-
-type Task = {
+type Coment = {
     name: string;
-    finished: boolean;
-
 };
 
-type Character = {
-    id: string,
-    title: string,
-    body: string
-}
-
 const Posts = () => {
-    const [tasks, setTasks] = useState<Task[]>([]);
-    const [inputTask, setInputTask] = useState('');
+    const [coment, setComent] = useState<Coment[]>([]);
+    const [inputComent, setInputComent] = useState('');
 
     const [onePost, setOnePost] = useState<{ id: string, title: string, body: string }>()
 
- // const history = useHistory()
+    // const history = useHistory()
+
 
     let { ids } = useParams<{ ids: string }>();
     console.log(ids)
 
+     localStorage.setItem('coment', JSON.stringify(coment))  
+    // @ts-ignore
+    let localComents = JSON.parse(localStorage.getItem('coment'))
+
     useEffect(() => {
-
-
-
         axios.get(`https://jsonplaceholder.typicode.com/posts/${ids}`)
             .then((response) => {
-                setOnePost(response.data)               
-              console.log(response.data)
+                setOnePost(response.data)
+                console.log(response.data)
 
-            //   if (!response.data) {
-            //     history.push("/404") 
-            // }
+                //   if (!response.data) {
+                //     history.push("/404") 
+                // }
             })
-           
-       
-            
     }, [])
 
-
     const inputChangeHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setInputTask(e.target.value);
+        setInputComent(e.target.value);
     };
-
     const buttonClickHandler = () => {
-        if (inputTask !== '') {
-            setTasks([
-                ...tasks,
+        if (inputComent !== '') {
+            setComent([
+                ...coment,
                 {
-                    name: inputTask,
-                    finished: false,
+                    name: inputComent
                 },]);
-            setInputTask('');
+            setInputComent('');
         }
     };
-    { localStorage.setItem('coment', JSON.stringify(tasks)) }
-    // @ts-ignore
-    let arr = JSON.parse(localStorage.getItem('coment'))
 
     return (
         <div className="constainer">
             <div className="row">
                 <div className="col-xs-12">
                     <div className="col-xs-offset-3 col-xs-6">
-
-                    
-
                         <h1>{onePost?.title}</h1>
                         <p>{onePost?.body}</p>
-
                         <div>
                             <h2>Comments</h2>
 
-                        {  // @ts-ignore} 
-                            arr.map((item) =>
-                                <div className="comment--out">
-                                    <p>{item.name}</p>
-                                </div>
-                            )}
+                            {  // @ts-ignore} 
+                                localComents.map((item) =>
+                                    <div className="comment--out">
+                                        <p>{item.name}</p>
+                                    </div>
+                                )}
                             <textarea className="comment--input"
                                 // type=""
-                                value={inputTask}
+                                value={inputComent}
                                 onChange={(e) => inputChangeHandler(e)}
                             />
                             <button className="comment--button" type="button" onClick={buttonClickHandler}>
                                 Add comment
-          </button>
+                             </button>
                         </div>
-
-
-
                     </div>
                 </div>
             </div>
-
-
-
-
         </div>
     )
 }
